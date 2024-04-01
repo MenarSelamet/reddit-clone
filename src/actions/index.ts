e0382a0653a3ce78591ba7baa3777194e032bb17
Menar Selamet <menarselamet007@gmail.com>
2024-04-01 23:57:44 +0300

    moving the sign-in/sign-out functions out of actions

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,14 +1,3 @@
-"use server";
-
-import * as auth from "@/auth";
-
-export async function signIn() {
-  return auth.signIn('github');
-}
-
-export async function signOut() {
-  return auth.signOut();
-}
 
 
 export { createComment } from './create-comment';
