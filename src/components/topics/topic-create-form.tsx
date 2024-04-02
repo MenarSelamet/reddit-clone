8a958284fb1ef4e63e9145aa992140bcbb939ce8
Menar Selamet <menarselamet007@gmail.com>
2024-04-02 07:32:16 +0300

    Create topic component initial design

--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -1,9 +1,30 @@
-
+import {
+  Button,
+  Input,
+  Textarea,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  popover,
+} from "@nextui-org/react";
 
 export default function TopicCreateForm() {
   return (
-    <div>
-      <h1>Topic Create Form</h1>
-    </div>
+    <Popover placement="bottom">
+      <PopoverTrigger>
+        <Button color="primary">Create Topic</Button>
+      </PopoverTrigger>
+      <PopoverContent>
+        <div className="p-0.5">
+          <form>
+            <Input placeholder="Title" />
+            <Textarea placeholder="Content" />
+            <Button type="submit" color="primary">
+              Create
+            </Button>
+          </form>
+        </div>
+      </PopoverContent>
+    </Popover>
   );
 }
