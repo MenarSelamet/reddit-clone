c66bdd6d725cea4f76357e924dd336fa47094f6e
Menar Selamet <menarselamet007@gmail.com>
2024-04-04 23:39:49 +0300

    Installed ZOD for validation

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,11 +1,22 @@
 "use server";
 
+import { z } from "zod";
+
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/^[a-z-]+$/, {
+      message: "Must be lowercase letter or dashes without spaces",
+    }),
+  description: z.string().min(10),
+});
+
 export async function createTopic(formData: FormData) {
-    const name = formData.get("name") as string;
-    const description = formData.get("description") as string;
+  const name = formData.get("name") as string;
+  const description = formData.get("description") as string;
 
-    console.log(name, description);
+  console.log(name, description);
 }
-
 
 //TODO: revalidate the homepage
